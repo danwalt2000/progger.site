48e5a13367efc2e515ad2d69722f496765f7b419
Vitalii Pozdniakov <pozdniakov@trueconf.ru>
2022-12-29 19:20:38 +0300

    projects looped on index page

--- resources/views/parts/myworks.blade.php
+++ resources/views/parts/myworks.blade.php
@@ -2,79 +2,32 @@
     <h2 class="introBlockHeading">Мои работы</h2>
     <div class="projectRow">
 
-        <div class="ProjectContentOuter">
-            <div class="ProjectContent">
-                <div>
-                    <section class="portfolio-block">
-                        <div class="portfolio-image-block">
-                            <a class="portfolio-image-block__link" href="novozarievka">
-                                <img src="content/novozar.png" alt="Сайт агрофирмы" class="projectImage" style="left:0;position: relative;top: 0;" />
-                            </a>
-                        </div>
-                        <div class="portfolio-content-block">
-                            <div class="portfolio-content-block__holder">
-                                <div class="portfolio-content-block__text">
-                                    <div class="portfolio-content-block__sub-title">Сайт агрофирмы</div>
-                                    <div class="portfolio-content-block__title">Новозариевка</div>
-                                    <a class="portfolio-content-block__link" title="Сайт агрофирмы" href="novozarievka">Подробнее</a>
-                                </div>
+        <div class="portfolio-grid">
+            @foreach ($projects as $project)
+                <section class="portfolio-block">
+                    <div class="portfolio-image-block">
+                        <a class="portfolio-image-block__link" href="{{ $project["link"] }}">
+                            <img src="{{ $project["img"] }}" alt="{{ $project["title"] }}"
+                                class="projectImage"
+                                style="left:0;position: relative;top: 0;" />
+                        </a>
+                    </div>
+                    <div class="portfolio-content-block">
+                        <div class="portfolio-content-block__holder">
+                            <div class="portfolio-content-block__text">
+                                <div class="portfolio-content-block__sub-title">
+                                    {{ $project["title"] }}</div>
+                                <div class="portfolio-content-block__title">
+                                    {{ $project["second_title"] }}</div>
+                                <a class="portfolio-content-block__link"
+                                    title="{{ $project["title"] }}"
+                                    href="{{ $project["link"] }}">Подробнее</a>
                             </div>
                         </div>
-                    </section>
-                </div>
-            </div>
+                    </div>
+                </section>
+            @endforeach
         </div>
-
-        <div class="ProjectContentOuter">
-            <div class="ProjectContent">
-                <div>
-                    <section class="portfolio-block">
-                        <div class="portfolio-image-block">
-                            <a class="portfolio-image-block__link" href="infotech">
-                                <img src="{{ asset('content/infotech.jpg') }}" alt="Infotech" class="projectImage"
-                                     style="left:0;position: relative;top: 0;"/>
-                            </a>
-                        </div>
-                        <div class="portfolio-content-block">
-                            <div class="portfolio-content-block__holder">
-                                <div class="portfolio-content-block__text">
-                                    <div class="portfolio-content-block__sub-title">Интернет-магазин</div>
-                                    <div class="portfolio-content-block__title">ЧП "ИНФОТЕХ"</div>
-                                    <a class="portfolio-content-block__link" title="Инфотех"
-                                       href="infotech">Подробнее</a>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </div>
-
-        <div class="ProjectContentOuter">
-            <div class="ProjectContent">
-                <div>
-                    <section class="portfolio-block">
-                        <div class="portfolio-image-block">
-                            <a class="portfolio-image-block__link" href="mebel">
-                                <img src="{{ asset('content/mebel.jpg') }}" alt="" class="projectImage"
-                                     style="left:0;position: relative;top: 0;"/>
-                            </a>
-                        </div>
-                        <div class="portfolio-content-block">
-                            <div class="portfolio-content-block__holder">
-                                <div class="portfolio-content-block__text">
-                                    <div class="portfolio-content-block__sub-title">Интернет-магазин</div>
-                                    <div class="portfolio-content-block__title">Мебель Донецк</div>
-                                    <a class="portfolio-content-block__link" title="Мебель Донецк"
-                                       href="mebel">Подробнее</a>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div class="buttonBlock">
         <div>
